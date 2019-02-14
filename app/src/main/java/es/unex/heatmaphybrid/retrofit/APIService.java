dc53319f72a904b61d8191a144f1adce2519853a
slasom <slasom@unex.es>
2019-02-14 14:08:58 +0100

    -Update account of Firebase (spilab).

--- app/src/main/java/es/unex/heatmaphybrid/retrofit/APIService.java
+++ app/src/main/java/es/unex/heatmaphybrid/retrofit/APIService.java
@@ -19,13 +19,6 @@ import retrofit2.http.Path;
 
 public interface APIService {
 
-//    @Headers({
-//            "Content-Type:application/json",
-//            "Authorization:key=AAAAXKsK2vU:APA91bHoA6hUn6DrrRGI9TBSvA7Zft3SQ9QOuuCR7R84eK_oLWy7K0eFUEf6OaNLQfiZkV8owSUHImntDx3rpZ2PVjQr8MqYawoIFJiwOlRMUHwl8qGKOyiTZS_bfmPT2HDi-zuPgC2p"
-//    })
-//    @POST( "fcm/send")
-//    Call<FirebaseResponse> sendNotification(@Body FirebaseSender body);
-
 
     @Headers(
             "Accept: application/json"
